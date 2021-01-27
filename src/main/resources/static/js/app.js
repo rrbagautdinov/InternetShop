d1d859438443f8bb080598173133b8b22b0587e5
urichalex <aurich@el-polis.com>
2021-01-27 22:07:11 +0300

    Routes to HTML5 history

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -7,3 +7,11 @@ var internetShop = angular.module('internet_shop', ['ngRoute', 'ngResource']).co
         .when('/users/page_:page', {templateUrl: '/templates/users.html', controller: 'usersController'})
         .otherwise({redirectTo: '/'});
 });
+internetShop.config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider){
+    if (window.history && window.history.pushState) { //проверка поддерживается ли бразуером html5 history API
+        $locationProvider.html5Mode({
+            enabled:     true,
+            requireBase: false
+        });
+    }
+}]);
